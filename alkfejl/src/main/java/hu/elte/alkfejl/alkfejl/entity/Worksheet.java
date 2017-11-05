de8fa1be663eafb8fe9355e6a316c9f85f9ee1c5
Milanmano <milanmano@gmail.com>
2017-11-05 13:48:33 +0100

    added entities

--- alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/entity/Worksheet.java
+++ alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/entity/Worksheet.java
@@ -1,12 +1,22 @@
 package hu.elte.alkfejl.alkfejl.entity;
 
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Worksheet")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Worksheet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
