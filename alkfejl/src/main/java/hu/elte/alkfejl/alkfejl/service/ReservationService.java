85dba8da14acf82861a02d0b2b73721f4bc2a670
Milanmano <milanmano@gmail.com>
2017-11-06 22:57:25 +0100

    Service TODO: services, repos

--- alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/service/ReservationService.java
+++ alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/service/ReservationService.java
@@ -1,12 +1,20 @@
 package hu.elte.alkfejl.alkfejl.service;
 
+import static com.oracle.jrockit.jfr.ContentType.Timestamp;
 import hu.elte.alkfejl.alkfejl.entity.Reservation;
 import hu.elte.alkfejl.alkfejl.entity.User;
+import hu.elte.alkfejl.alkfejl.entity.User.Role;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ReservationService {
 
-    public Reservation read(int parseInt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Autowired
+    private ReservationRepository reservationRepository;
+    
+    
+    public Reservation read(int id) {
+        return reservationRepository.findOne(id);
     }
 
     public Reservation update(long id, Reservation reservation) {
@@ -14,15 +22,26 @@ public class ReservationService {
     }
 
     public void delete(long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        reservationRepository.delete(id);
     }
 
-    public Reservation create(Reservation reservation, User loggedInUser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Reservation create(Reservation reservation, User user) {
+        //stufftodo
+        return reservationRepository.save(reservation);
     }
 
-    public Iterable<Reservation> listByRole(User loggedInUser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Iterable<Reservation> listByRole(User user) {
+        Role role = user.getRole();
+        if (role == Role.PARTNER) {
+            return reservationRepository.findAllByUser(user);
+        } 
+        else if (role == Role.ADMIN) {
+            return reservationRepository.findAll();
+        }
+        else if (role == Role.WORKER) {
+            return reservationRepository.findAllByWorker();
+        }
+        return Collections.emptyList();
     }
     
 }
--- alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/service/UserService.java
+++ alkfejl/src/main/java/hu/elte/alkfejl/alkfejl/service/UserService.java
@@ -1,27 +1,45 @@
 package hu.elte.alkfejl.alkfejl.service;
 
 import hu.elte.alkfejl.alkfejl.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
+@Service
+@SessionScope
 public class UserService {
 
+    private User user;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
     public boolean isLoggedIn() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return user != null;
     }
 
     public User register(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        user.setRole(User.Role.PARTNER);
+        return this.user = userRepository.save(user);
     }
 
     public void logout() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        user = null;
     }
 
-    public User login(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public User login(User user) throws UserNotValidException{
+        if (isValid(user)) {
+            return this.user = userRepository.findByUsername(user.getUsername());
+        }
+        throw new UserNotValidException();
     }
-
+    
+    private boolean isValid(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()).isPresent();
+    }
+    
     public User getLoggedInUser() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return user;
     }
     
 }
